bbfcaa3e21a8630d5a6e9aa5d00ca6bf128bec52
greccy <greccy.verde@gmail.com>
2025-08-17 14:57:01 +0200

    Validación de sorteo

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+let amigoSorteado = '';
 
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
+
+    if (/\d/.test(nombreAmigo)) {
+        //Validación para que el nombre no contenga números 
+        alert("El nombre no puede contener números.");
+        return;
+    }
     if (nombreAmigo == '') {
         alert("Por favor, escribe el nombre de un amigo.");
         return;
@@ -12,13 +19,6 @@ function agregarAmigo() {
         alert("El nombre debe tener al menos 3 caracteres.");
         return;
     }
-
-    if (/\d/.test(nombreAmigo)) {
-        //Validación para que el nombre no contenga números 
-        alert("El nombre no puede contener números.");
-        return;
-    }
-
     amigos.push(nombreAmigo);
     actualizarListaAmigos();
     document.getElementById('amigo').value = '';
@@ -28,7 +28,7 @@ function agregarAmigo() {
 
 function actualizarListaAmigos() {
     let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = ''; 
+    lista.innerHTML = '';
 
     for (let i = 0; i < amigos.length; i++) {
         // Crea un elemento de lista para cada amigo
@@ -39,12 +39,18 @@ function actualizarListaAmigos() {
 }
 
 function sortearAmigo() {
-    if (amigos.length === 0) {
-        alert("No hay amigos para sortear.");
-        return;
+    if (amigoSorteado == '') {
+        if (amigos.length === 0) {
+            alert("No hay amigos para sortear.");
+            return;
+        }
+        amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById('listaAmigos').innerHTML = ''; // Limpiar la lista antes de mostrar el resultado
+        let resultado = document.getElementById('resultado'); // Aquí es donde se actualiza el elemento 'resultado'
+        resultado.innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+
+    } else {
+        resultado.innerHTML = `ya has seleccionado a tu amigo secreto, es: ${amigoSorteado}`;
     }
-    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];      
-    document.getElementById('listaAmigos').innerHTML = ''; // Limpiar la lista antes de mostrar el resultado
-    let resultado = document.getElementById('resultado'); // Aquí es donde se actualiza el elemento 'resultado'
-    resultado.innerHTML = `El amigo secreto es: ${amigoSorteado}`;    
+
 }
